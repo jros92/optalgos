accaaa542c063f8d74641c382ac8843c52223204
Jörg Schmidt <jroschmidt@gmail.com>
2018-03-30 15:05:42 +0200

    Worked on Objective Function

--- src/core/NeighborhoodGeometric.java
+++ src/core/NeighborhoodGeometric.java
@@ -1,40 +1,47 @@
-///**
-// *
-// */
-//package core;
-//
-//import algorithms.INeighborhood;
-//import algorithms.Neighborhood;
-//
-///**
-// * Geometric Neighborhood for the 'rectangle fitting' problem
-// * @author Jörg R. Schmidt <jroschmidt@gmail.com>
-// *
-// */
-//public class NeighborhoodGeometric extends Neighborhood implements INeighborhood {
-//
-//	public NeighborhoodGeometric() {
-//		super("Geometric");
-//	}
-//
-//	/**
-//	 * Return a specified number of neighbors as an array
-//	 * @param solution
-//	 * @param numberOfNeighbors
-//	 * @return
-//	 */
+/**
+ *
+ */
+package core;
+
+import algorithms.INeighborhood;
+import algorithms.Neighborhood;
+
+/**
+ * Geometric Neighborhood for the 'rectangle fitting' problem
+ * @author Jörg R. Schmidt <jroschmidt@gmail.com>
+ *
+ */
+public class NeighborhoodGeometric extends Neighborhood implements INeighborhood {
+
+	public NeighborhoodGeometric() {
+		super("Geometric");
+	}
+
+
+    @Override
+    public FeasibleSolution getNeighbor(FeasibleSolution solution) {
+        return null;
+    }
+
+	/**
+	 * Return a specified number of neighbors as an array
+	 * @param solution
+	 * @param numberOfNeighbors
+	 * @return
+	 */
 //	@Override
-//	public FeasibleSolution[] getNeighbors(FeasibleSolution solution, int numberOfNeighbors) {
-//		FeasibleSolution[] solutions = new FeasibleSolution[numberOfNeighbors];
-//
-//		Rectangle rect0 = solution.getBoxes().get(0).getRectangles().get(0);
-//
-//		rect0.moveRight();
-//
-//		solutions[0] = solution;
-//
-//		return solutions;
-//	}
-//
-//
-//}
+	public FeasibleSolution[] getNeighbors(FeasibleSolution solution, int numberOfNeighbors) {
+		FeasibleSolution[] solutions = new FeasibleSolution[numberOfNeighbors];
+
+		Rectangle rect0 = solution.getBoxes().get(0).getRectangles().get(0);
+
+		rect0.moveRight();
+
+		solutions[0] = solution;
+
+		return solutions;
+	}
+
+
+
+}
--- src/core/ObjectiveFunction.java
+++ src/core/ObjectiveFunction.java
@@ -8,16 +8,34 @@ public class ObjectiveFunction implements IObjectiveFunction {
 	public double getValue(FeasibleSolution solution) {
 //		System.out.println("Current Box Count: " + solution.getBoxCount());
 //		return solution.getBoxCount();
-		
-		float[] weights = new float[] {0.7f, 0.3f};
-		
-		double result = 0d;
-		
-		result += (double)solution.getBoxCount() * (double)weights[0];
-		result += (double)solution.getBoxCount() * (double)weights[1];
+
+		int boxCount = solution.getBoxCount();
+
+		double weightedCumulativePackingPercentage = 0;
+		for (Box box : solution.getBoxes()) {
+			weightedCumulativePackingPercentage += (double)box.getRectangles().size() * (double)box.getOccupiedArea();
+		}
+
+		System.out.println("[OBJFUN] weightedCumulativePackingPercentage = " + weightedCumulativePackingPercentage);
+
+		double[] weights = new double[] {0.9f, 0.1f};
+//		double[] weights = new double[2];
+//		weights[1] = 1f / (1f + weightedCumulativePackingPercentage / (double)boxCount);
+//		weights[0] = 1f - weights[1];
+
+		System.out.println("[OBJFUN] Weights = [" + weights[0] + ", " + weights[1] + "]");
+
+		double result;
+		result = (double)boxCount * (double)weights[0];
+		result -= weightedCumulativePackingPercentage * weights[1];
+
+		result = -weightedCumulativePackingPercentage;
+
 		//TODO: Use avg. covered area
-		// result -= (double)solution.calculcateCumulativePackingPercentage() * (double)weights[1];
-		
+		// result -= (double)solution.calculateCumulativePackingPercentage() * (double)weights[1];
+
+
+
 //		System.out.println("Objective Function Value: " + result);
 		
 		return result;
